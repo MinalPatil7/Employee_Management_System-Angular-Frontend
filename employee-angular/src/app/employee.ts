d721a83cfe8a072f525bea93d5d825c490b75b37
MinalPatil7 <minalpatil2507@gmail.com>
2022-12-19 07:11:50 +0530

    first commit

---
+++ employee-angular/src/app/employee.ts
@@ -0,0 +1,15 @@
+export class Employee {
+    id: number;
+    firstName: String;
+    lastName: String;
+    emailId: String;
+    active: boolean;
+  
+    // constructor(id: number,firstName:String,lastName:String,emailId:String,){
+    //     this.id= id;
+    //     this.firstName = firstName;
+    //     this.lastName=lastName;
+    //     this.emailId= emailId;
+
+    // }
+}
